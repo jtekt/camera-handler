e82d0b38761a3023b88e3a4b914d739f7b219e52
Maxime MOREILLON <maxime_moreillon@jtekt.co.jp>
2021-04-13 11:40:56 +0900

    Reworked camera settings routes

--- routes/settings.py
+++ routes/settings.py
@@ -1,28 +1,17 @@
 from fastapi import APIRouter
 from controller import camera_helper
 from model.camera import Configuration
-from routes.root import can
 
-router = APIRouter(prefix="/camera")
-
-
-@router.get("/restart")
-async def restart():
-    try:
-        can.stop_camera()
-        can.start_camera()
-        return {"message": "Camera has been restarted"}
-    except Exception as e:
-        return {"message": "Camera restart failed", "detail": str(e)}
+router = APIRouter(prefix="/settings")
 
 
-@router.get("/settings")
+@router.get("/")
 async def get_camera_settings():
     settings = camera_helper.get_camera_settings()
     return settings
 
 
-@router.patch("/settings")
+@router.patch("/")
 async def configure_camera(configuration: Configuration = {}):
     configuration = configuration.dict(exclude_unset=True)
 
